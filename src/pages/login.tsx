966d6af047ccc2b9d72a1ec5239473cb6e376362
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-10-02 16:02:39 +0900

    login css

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,7 +13,7 @@ export const Login = () => {
         isLoggedInVar(true);
     }
 
-    const {watch, getValues, handleSubmit, register} = useForm<IForm>()
+    const {watch, getValues, handleSubmit, register, formState:{errors}} = useForm<IForm>()
 
     const onSubmit = () => {
         console.log(getValues())
@@ -21,20 +21,37 @@ export const Login = () => {
 
     return(
         <div className='bg-gray-700 h-screen flex justify-center items-center'>
-            <div className='w-full max-w-lg rounded-lg bg-white text-center py-10'>
+            <div className='w-full max-w-lg rounded-lg bg-white text-center pt-8 pb-3'>
                 <h3 className='font-semibold text-2xl mb-5'>Log In</h3>
-                <form className='flex flex-col px-5 py-3' onSubmit={handleSubmit(onSubmit)}>
+                <form className='grid gap-3 px-5 py-3' onSubmit={handleSubmit(onSubmit)}>
                     <input
-                     {...register("email", {required: true})}
+                     {...register("email", {required: `Email is required`})}
                      placeholder="example@emaple.com"
-                     className="px-5 py-3 mb-3 shadow-inner bg-gray-100 border-2 focus:border-indigo-700 focus:border-opacity-50 focus:outline-none rounded-lg "
+                     type="email"
+                     className="input" 
                     />
+                    {errors.email?.message && (
+                        <span className='error'>
+                            {errors.email.message}
+                        </span>
+                    )}
                     <input
-                     {...register("password", {required: true})}
+                     {...register("password", {required: `Password is required`, minLength:3})}
                      placeholder="password"
-                     className="px-5 py-3 mb-3 shadow-inner bg-gray-100 border-2 focus:border-indigo-700 focus:border-opacity-50 focus:outline-none rounded-lg "
+                     type="password"
+                     className="input" 
                     />
-                    <button onClick={onClick} className='bg-indigo-600 hover:bg-indigo-700 text-lg text-white font-normal mt-2 py-3 rounded-lg focus:outline-none'>Log In</button>
+                    {errors.password?.message && (
+                        <span className='error'>
+                            {errors.password.message}
+                        </span>
+                    )}
+                    {errors.password?.type === "minLength" && (
+                        <span className='error'>
+                            Password must be more than 3 chars.
+                        </span>
+                    )}
+                    <button  className='bg-indigo-600 hover:bg-indigo-700 text-lg text-white font-normal mt-2 py-3 rounded-lg focus:outline-none'>Log In</button>
                 </form>
             </div>
         </div>
