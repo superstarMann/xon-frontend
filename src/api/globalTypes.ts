adae277ce79e80ac88c7c798cba98b55dcdcfc06
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2022-02-28 15:57:52 +0900

    build

---
+++ src/api/globalTypes.ts
@@ -0,0 +1,334 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum CountrySelect {
+  Afghanistan = "Afghanistan",
+  Albania = "Albania",
+  Algeria = "Algeria",
+  Andorra = "Andorra",
+  Angola = "Angola",
+  Antigua_and_Barbuda = "Antigua_and_Barbuda",
+  Argentina = "Argentina",
+  Armenia = "Armenia",
+  Australia = "Australia",
+  Austria = "Austria",
+  Azerbaijan = "Azerbaijan",
+  Bahrain = "Bahrain",
+  Bangladesh = "Bangladesh",
+  Barbados = "Barbados",
+  Belarus = "Belarus",
+  Belgium = "Belgium",
+  Belize = "Belize",
+  Benin = "Benin",
+  Bhutan = "Bhutan",
+  Bolivia = "Bolivia",
+  Bosniaand_Herzegovina = "Bosniaand_Herzegovina",
+  Botswana = "Botswana",
+  Brazil = "Brazil",
+  Brunei = "Brunei",
+  Bulgaria = "Bulgaria",
+  BurkinaFaso = "BurkinaFaso",
+  Burma = "Burma",
+  Burundi = "Burundi",
+  Cambodia = "Cambodia",
+  Cameroon = "Cameroon",
+  Canada = "Canada",
+  CapeVerde = "CapeVerde",
+  Central_African_Republic = "Central_African_Republic",
+  Chad = "Chad",
+  Chile = "Chile",
+  China = "China",
+  Colombia = "Colombia",
+  Comoros = "Comoros",
+  Costa_Rica = "Costa_Rica",
+  Cote_dIvoire = "Cote_dIvoire",
+  Croatia = "Croatia",
+  Cuba = "Cuba",
+  Cyprus = "Cyprus",
+  Czech_Republic = "Czech_Republic",
+  Democratic_Republic_of_theCongo = "Democratic_Republic_of_theCongo",
+  Denmark = "Denmark",
+  Djibouti = "Djibouti",
+  Dominica = "Dominica",
+  Dominican_Republic = "Dominican_Republic",
+  East_Timor = "East_Timor",
+  Ecuador = "Ecuador",
+  Egypt = "Egypt",
+  El_Salvador = "El_Salvador",
+  Equatorial_Guinea = "Equatorial_Guinea",
+  Eritrea = "Eritrea",
+  Estonia = "Estonia",
+  Ethiopia = "Ethiopia",
+  Fiji = "Fiji",
+  Finland = "Finland",
+  France = "France",
+  Gabon = "Gabon",
+  Georgia = "Georgia",
+  Germany = "Germany",
+  Ghana = "Ghana",
+  Greece = "Greece",
+  Grenada = "Grenada",
+  Guatemala = "Guatemala",
+  Guinea_Bissau = "Guinea_Bissau",
+  Guyana = "Guyana",
+  Haiti = "Haiti",
+  Holy_See = "Holy_See",
+  Honduras = "Honduras",
+  Hong_Kong = "Hong_Kong",
+  Hungary = "Hungary",
+  Iceland = "Iceland",
+  India = "India",
+  Indonesia = "Indonesia",
+  Iran = "Iran",
+  Iraq = "Iraq",
+  Ireland = "Ireland",
+  Israel = "Israel",
+  Italy = "Italy",
+  Jamaica = "Jamaica",
+  Japan = "Japan",
+  Jordan = "Jordan",
+  K = "K",
+  Kazakhstan = "Kazakhstan",
+  Kenya = "Kenya",
+  Kiribati = "Kiribati",
+  Kosovo = "Kosovo",
+  Kuwait = "Kuwait",
+  Kyrgyzstan = "Kyrgyzstan",
+  Laos = "Laos",
+  Latvia = "Latvia",
+  Lebanon = "Lebanon",
+  Lesotho = "Lesotho",
+  Liberia = "Liberia",
+  Libya = "Libya",
+  Liechtenstein = "Liechtenstein",
+  Lithuania = "Lithuania",
+  Luxembourg = "Luxembourg",
+  Macau = "Macau",
+  Macedonia = "Macedonia",
+  Madagascar = "Madagascar",
+  Malawi = "Malawi",
+  Malaysia = "Malaysia",
+  Maldives = "Maldives",
+  Mali = "Mali",
+  Malta = "Malta",
+  Marshall_Islands = "Marshall_Islands",
+  Mauritania = "Mauritania",
+  Mauritius = "Mauritius",
+  Mexico = "Mexico",
+  Micronesia = "Micronesia",
+  Moldova = "Moldova",
+  Monaco = "Monaco",
+  Mongolia = "Mongolia",
+  Montenegro = "Montenegro",
+  Morocco = "Morocco",
+  Mozambique = "Mozambique",
+  Namibia = "Namibia",
+  Nauru = "Nauru",
+  Nepal = "Nepal",
+  Nether_lands = "Nether_lands",
+  Netherlands_Antilles = "Netherlands_Antilles",
+  New_Zealand = "New_Zealand",
+  Nicaragua = "Nicaragua",
+  Niger = "Niger",
+  Nigeria = "Nigeria",
+  North_Korea = "North_Korea",
+  Norway = "Norway",
+  Oman = "Oman",
+  Pakistan = "Pakistan",
+  Palau = "Palau",
+  Palestinian_Territories = "Palestinian_Territories",
+  Panama = "Panama",
+  Papua_New_Guinea = "Papua_New_Guinea",
+  Paraguay = "Paraguay",
+  Peru = "Peru",
+  Philippines = "Philippines",
+  Poland = "Poland",
+  Portugal = "Portugal",
+  Qatar = "Qatar",
+  Republic_of_the_Congo = "Republic_of_the_Congo",
+  Romania = "Romania",
+  Russia = "Russia",
+  Rwanda = "Rwanda",
+  Saint_Kitts_and_Nevis = "Saint_Kitts_and_Nevis",
+  Saint_Lucia = "Saint_Lucia",
+  Saint_Vincent_and_the_Grenadines = "Saint_Vincent_and_the_Grenadines",
+  Samoa = "Samoa",
+  San_Marino = "San_Marino",
+  Sao_Tome_and_Principe = "Sao_Tome_and_Principe",
+  Saudi_Arabia = "Saudi_Arabia",
+  Senegal = "Senegal",
+  Serbia = "Serbia",
+  Seychelles = "Seychelles",
+  Sierra_Leone = "Sierra_Leone",
+  Singapore = "Singapore",
+  Slovakia = "Slovakia",
+  Slovenia = "Slovenia",
+  Solomon_Islands = "Solomon_Islands",
+  Somalia = "Somalia",
+  South_Africa = "South_Africa",
+  South_Korea = "South_Korea",
+  South_Sudan = "South_Sudan",
+  Spain = "Spain",
+  Sri_Lanka = "Sri_Lanka",
+  Sudan = "Sudan",
+  Suriname = "Suriname",
+  Swaziland = "Swaziland",
+  Sweden = "Sweden",
+  Switzerland = "Switzerland",
+  Syria = "Syria",
+  Taiwan = "Taiwan",
+  Tajikistan = "Tajikistan",
+  Tanzania = "Tanzania",
+  Thailand = "Thailand",
+  TheBahamas = "TheBahamas",
+  The_Gambia = "The_Gambia",
+  Timor_Leste = "Timor_Leste",
+  Togo = "Togo",
+  Tonga = "Tonga",
+  Trinidad_and_Tobago = "Trinidad_and_Tobago",
+  Tunisia = "Tunisia",
+  Turkey = "Turkey",
+  Turkmenistan = "Turkmenistan",
+  Tuvalu = "Tuvalu",
+  Uganda = "Uganda",
+  Ukraine = "Ukraine",
+  United_Arab_Emirates = "United_Arab_Emirates",
+  United_Kingdom = "United_Kingdom",
+  Uruguay = "Uruguay",
+  Uzbekistan = "Uzbekistan",
+  Vanuatu = "Vanuatu",
+  Venezuela = "Venezuela",
+  Vietnam = "Vietnam",
+  Yemen = "Yemen",
+  Zambia = "Zambia",
+  Zimbabwe = "Zimbabwe",
+}
+
+export enum OrderStatus {
+  Arrived = "Arrived",
+  Going = "Going",
+  Pending = "Pending",
+  Protecting = "Protecting",
+}
+
+export enum UserRole {
+  Guader = "Guader",
+  User = "User",
+}
+
+export interface CountryInput {
+  page?: number | null;
+  slug: string;
+}
+
+export interface CreateAccountInput {
+  email: string;
+  password: string;
+  role: UserRole;
+  countrySelect?: CountrySelect | null;
+}
+
+export interface CreateDishInput {
+  name: string;
+  price: number;
+  description: string;
+  options?: DishOptionInputType[] | null;
+  shareMusleId: number;
+}
+
+export interface CreateOrderInput {
+  shareMusleId: number;
+  items: CreateOrderItemInput[];
+}
+
+export interface CreateOrderItemInput {
+  dishId: number;
+  options?: OrderItemOptionInput[] | null;
+}
+
+export interface CreateShareMusleInput {
+  name: string;
+  address: string;
+  coverImg: string;
+  countryName: string;
+}
+
+export interface DeleteDishInput {
+  dishId: number;
+}
+
+export interface DeleteOrderInput {
+  orderId: number;
+}
+
+export interface DishChoiceInputType {
+  name: string;
+  extra?: number | null;
+}
+
+export interface DishOptionInputType {
+  name: string;
+  choices?: DishChoiceInputType[] | null;
+  extra?: number | null;
+}
+
+export interface EditOrderInput {
+  id: number;
+  status: OrderStatus;
+}
+
+export interface EditProfileInput {
+  email?: string | null;
+  password?: string | null;
+  role?: UserRole | null;
+  countrySelect?: CountrySelect | null;
+}
+
+export interface GetOrderInput {
+  id: number;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export interface MyShareMusleInput {
+  id: number;
+}
+
+export interface OrderItemOptionInput {
+  name: string;
+  choice?: string | null;
+}
+
+export interface SearchShareMusleInput {
+  page?: number | null;
+  query: string;
+}
+
+export interface ShareMusleInput {
+  shareMusleId: number;
+}
+
+export interface ShareMuslesInput {
+  page?: number | null;
+}
+
+export interface UpdateOrderInput {
+  id: number;
+}
+
+export interface VerifyEmailInput {
+  code: string;
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
